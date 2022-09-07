7f1d58383fce19de29db8b39a7282335a3cf6c36
Rajarajacholan Krishnamurthy <rajarajacholan.krishnamurthy@bwdesigngroup.com>
2022-09-07 22:18:25 +0530

    command line parameter

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
-//callback chaining
-geocode ('Chennai', (error, data) => {
-    if (error)
-       console.log('Error: ', error)
-    
-    console.log('Data: ' , data)
-    forecast(data.latitude, data.longitude, (error, data) => {
+const location = process.argv[2]
+
+if (!location)
+    console.log('Please provide location')
+else
+{
+    //callback chaining
+    geocode (location, (error, data) => {
         if (error)
-            console.log('Error: ', error)
-        console.log('Data: ', data)
+        console.log('Error: ', error)
+        else
+        {
+            console.log('Data: ' , data)
+            forecast(data.latitude, data.longitude, (error, forecastdata) => {
+                if (error)
+                    console.log('Error: ', error)
+                console.log('Data: ', forecastdata)
+            })
+        }
     })
-})
+}
